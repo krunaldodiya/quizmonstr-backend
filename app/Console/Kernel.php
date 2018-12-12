248e0d1bfe4fbb437076dc21f95b67492d16d34c
krunaldodiya <kunal.dodiya1@gmail.com>
2018-12-12 10:42:46 +0530

    done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('create:quiz')->hourly();
+        $schedule->command('create:quiz')->everyFifteenMinutes();
     }
 
     /**
